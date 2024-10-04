8cec84a5b0d16b100188932e14049ec3c6725be4
OsamaASidd <k200216@nu.edu.pk>
2024-10-05 01:03:26 +0500

    Latest working Api

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-from flask import Flask, jsonify, request
-from app.routers import liquidation
-from dotenv import load_dotenv
-
-app = Flask(__name__)
-
-app.register_blueprint(liquidation.bp)
